f4fb4170ff24f1c5d6964ea36d53ef5563912296
Gerard.Perujo <ger.pe85@gmail.com>
2023-10-22 19:11:18 +0200

    primer commit

---
+++ src/main/java/unir/Ad1/Ad1EntornoServidorApplication.java
@@ -0,0 +1,13 @@
+package unir.Ad1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ad1EntornoServidorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ad1EntornoServidorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/unir/Ad1/Ad1EntornoServidorApplicationTests.java
@@ -0,0 +1,13 @@
+package unir.Ad1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ad1EntornoServidorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
